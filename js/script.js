b8ecd7567b73e2339f6120a80374f68a2d3b170a
Xinwei Ge <xinwei.ge97@gmail.com>
2020-08-16 00:18:20 -0400

    loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+$(window).on("load", function() {
+
+    $(".loader .inner").fadeOut(1000, function() {
+        $(".loader").fadeOut(500)
+    });
+})
+
+
+
+
 $(document).ready(function(){
 
     $('#slides').superslides({
@@ -96,6 +106,33 @@ $(document).ready(function(){
 
     })
 
-    
+    $("#navigation li a").click(function(e) {
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html,body").animate({scrollTop: targetPosition - 50}, "slow");
+    });
+
+    const nav =$("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        var body = $("body");
+        
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        }
+
+        else {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
+    }
+
     
 });
